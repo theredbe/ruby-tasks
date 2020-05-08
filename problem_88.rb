4d1209a4873aa959f6aa819716c72130c12a246e
theredbe <morelessequal@gmail.com>
2020-05-08 16:37:28 +0300

    fix: problem 88

--- problem_88.rb
+++ problem_88.rb
@@ -1,19 +1,21 @@
-$kmax = 5
-
-if $kmax > 12
-  $kmax += 1
-end
-
-$n = 2*$kmax * $kmax
+$kmax = 12000
+$kmax += 1 if $kmax > 12
 
 def prodsum(p, s, c, start)
   k = p - s + c
+
   if k < $kmax
     if p < $n[k]
       $n[k] = p
-      (start..($kmax/p*2).to_f + 1).each do
-        prodsum(p*i, s+i, c+1, i)
-      end
+    end
+
+    (start..$kmax % p*2 + 1).each do |i|
+      prodsum(p*i, s+i, c+1, i)
     end
   end
 end
+
+$n =[2*$kmax] * $kmax
+prodsum(1, 1, 1, 2)
+
+puts $n.last
