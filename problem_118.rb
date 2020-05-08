4abedb2057f80cf2a089bf5e5aec5fbc4ccacd7c
theredbe <morelessequal@gmail.com>
2020-05-08 09:56:11 +0300

    feat: problem 118

--- problem_118.rb
+++ problem_118.rb
@@ -1,22 +1,75 @@
 require 'prime'
 
-perm = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+$perm = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-def partions(startIndex, prev)
-  count = 0
-  
-  for i in (startIndex, perm.length()) do
-    number = 0
-    for j in (startIndex, i) do
-      number = number * 10 + perm[j]
-    end
+def swap(i, j)
+  temp = $perm[i]
+  $perm[i] = $perm[j]
+  $perm[j] = temp
+end
 
-    next if (number < prev)
-    next if number.isPrime?
-    return count + 1 if (i == (perm.length() - 1))
+def permutation()
+  n = $perm.length()
+  i = n - 1
 
-    count += partions(i + 1, number)
+  while $perm[i-1] >= $perm[i] do
+    i -= 1
+    return false if i == 0
   end
 
-  return count
+  j = n
+
+  while $perm[j-1] <= $perm[i-1] do
+    j -= 1
+  end
+
+  swap(i - 1, j - 1)
+
+  i += 1
+  j = n
+
+  while i < j do
+    swap(i - 1, j - 1)
+    i += 1
+    j -= 1
+  end
+
+  return true
 end
+
+def partions(startIndex, prev)
+  begin
+    count = 0
+
+    (startIndex..$perm.length()-1).each do |i|
+      number = 0
+
+      (startIndex..i).each do |j|
+        number = number * 10 + $perm[j]
+      end
+
+      next if number < prev
+      next if !Prime.prime?(number)
+
+      if i == ($perm.length()-1)
+        return count + 1
+      end
+
+      count += partions(i + 1, number)
+    end
+
+    return count
+
+  rescue TypeError => e
+    puts 'Type Error was occured: ', err.message
+  end
+end
+
+sets = 0
+loop do
+  sets += partions(0, 0)
+break if !permutation()
+
+end
+
+puts 'There are ', sets, ' sets'
